196c19ff388a6ca23e014ba0c62da3c7053dabd3
tauthdev <tauthdev@gmail.com>
2024-11-25 19:14:48 +0900

    fix : redissonclient

--- src/main/kotlin/com/tripleauth/hypriority/config/HypriorityConfig.kt
+++ src/main/kotlin/com/tripleauth/hypriority/config/HypriorityConfig.kt
@@ -1,18 +1,10 @@
 package com.tripleauth.hypriority.config
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect
-import com.fasterxml.jackson.annotation.PropertyAccessor
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.tripleauth.hypriority.manager.HypriorityManager
 import com.tripleauth.hypriority.processor.HypriorityListenerProcessor
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
-import org.redisson.client.codec.Codec
-import org.redisson.codec.JsonJacksonCodec
 import org.redisson.config.Config
 import org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE
 import org.springframework.context.annotation.Bean
@@ -33,19 +25,8 @@ class HypriorityConfig(
         val config = Config()
         val redisAddress = "redis://${properties.host}:${properties.port}"
 
-        val objectMapper = jacksonObjectMapper().apply {
-            setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
-            disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-            deactivateDefaultTyping()
-        }
-
-        val codec: Codec = JsonJacksonCodec(objectMapper)
-
         config.useSingleServer()
-            .address = redisAddress
-        config.codec = codec
-
-        config.useSingleServer()
+            .setAddress(redisAddress)
             .setDatabase(properties.database)
             .setConnectionPoolSize(properties.connectionPoolSize)
             .setConnectionMinimumIdleSize(properties.connectionMinimumIdleSize)
