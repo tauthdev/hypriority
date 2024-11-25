073ccd84ac9a4a8cafef3e14c45ef38a5e2179b3
tauthdev <tauthdev@gmail.com>
2024-11-25 15:38:04 +0900

    modify : redis client object mapper

--- src/main/kotlin/com/tripleauth/hypriority/config/HypriorityConfig.kt
+++ src/main/kotlin/com/tripleauth/hypriority/config/HypriorityConfig.kt
@@ -1,16 +1,24 @@
 package com.tripleauth.hypriority.config
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.PropertyAccessor
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.tripleauth.hypriority.manager.HypriorityManager
 import com.tripleauth.hypriority.processor.HypriorityListenerProcessor
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.redisson.Redisson
 import org.redisson.api.RedissonClient
+import org.redisson.client.codec.Codec
+import org.redisson.codec.JsonJacksonCodec
 import org.redisson.config.Config
 import org.springframework.beans.factory.config.BeanDefinition.ROLE_INFRASTRUCTURE
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Role
 import org.springframework.context.annotation.DependsOn
+import org.springframework.context.annotation.Role
 
 @Configuration
 class HypriorityConfig(
@@ -25,8 +33,22 @@ class HypriorityConfig(
         val config = Config()
         val redisAddress = "redis://${properties.host}:${properties.port}"
 
+        val objectMapper = jacksonObjectMapper().apply {
+            setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY)
+            activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL
+            )
+            disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+        }
+
+        val codec: Codec = JsonJacksonCodec(objectMapper)
+
         config.useSingleServer()
-            .setAddress(redisAddress)
+            .address = redisAddress
+        config.codec = codec
+
+        config.useSingleServer()
             .setDatabase(properties.database)
             .setConnectionPoolSize(properties.connectionPoolSize)
             .setConnectionMinimumIdleSize(properties.connectionMinimumIdleSize)
